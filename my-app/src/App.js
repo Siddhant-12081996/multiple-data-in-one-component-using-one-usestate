3d0752436512f5b70c1c68ae5cfbebade57ef8c5
Siddhant-12081996 <siddhantbobde7425@gmail.com>
2022-12-04 13:15:15 +0530

    multiple data in useState

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Multiple from './components/Multiple';
+
+function App() {
+  
+
+  return (
+   <div>
+  
+  <Multiple/>
+    </div>
+  );
+}
+
+export default App;
